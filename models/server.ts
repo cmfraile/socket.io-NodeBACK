3690e40637709e15e9d3cee4021c64051cb5d1cb
Carlos Fraile <carlosfrailegomez93@gmail.com>
2021-12-30 12:14:51 +0100

    202

--- models/server.ts
+++ models/server.ts
@@ -1,19 +1,27 @@
 import express , { Application } from 'express';
+import cors , {CorsOptions} from 'cors';
 
 class Server {
 
     private app:Application;
     private port:string;
+    private coptions:CorsOptions = {origin:['http://localhost:8000','http://localhost:4200']};
 
     constructor(){
         this.app = express();
         this.port = process.env.PORT || '8000';
+        this.middlewares();
+    }
+
+    middlewares(){
+        this.app.use(express.json());
+        this.app.use(cors(this.coptions));
+        this.app.use(express.static('public'));
     }
 
     listen(){
         this.app.listen(this.port, () => {
-            console.log(`Servidor corriendo en puerto ${this.port}`);
-            console.log("tsc --watch && nodemon dist/app.js &&");
+            console.log(`CORRIENDO EN ${this.port}`);
         })
     }
 
